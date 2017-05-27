043ced4391fd1a05ef3453e519f7ad246b2e74e7
Grant Marvin <rogueg@gmail.com>
2017-05-27 12:22:56 -0700

    Get a stack trace to the timed out callback

--- example/tests.js
+++ example/tests.js
@@ -1,6 +1,9 @@
 window.expect = chai.expect
 
 describe('example', () => {
+  // before callback that never resolves
+  // before(() => { return new Promise((r) => {}) })
+
   it('can do math', () => {
     expect(1).to.equal(1)
   })
