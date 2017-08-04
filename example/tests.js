72fd996476705a88b0488dd60340e2b3fee45092
rogueg <grant@superhuman.com>
2017-08-03 19:09:39 -0700

    Latte: handle multiple (nested) suites
    In order to split tests between instances, we need those instances to be able to run arbitrary sets of tests.
    
    Instead of providing a suite name, Latte now accepts a `selection` function that is given the tree of suites, and a flat list of every test. This should let Zen code divvy things up as it sees fit.
    
    The other major part is handing before/after callbacks as we traverse up and down describe trees.

--- example/tests.js
+++ example/tests.js
@@ -20,13 +20,47 @@ describe('example', () => {
   // before callback that never resolves
   // before(() => { return new Promise((r) => {}) })
 
-  it('can do math', () => {
-    expect(1).to.equal(1)
+  before(() => { console.log('before example') })
+  after(() => { console.log('after example') })
+  beforeEach(() => console.log('beforeEach example'))
+  afterEach(() => console.log('afterEach example'))
+
+  helper('echoBot', {
+    welcome: function() { return "Hello " + this.name }
   })
 
-  it('doesnt timeout on breakpoints', () => {
-    expect(1).to.equal(1)
-    debugger
-    expect(1).to.equal(1)
+  it('can do math', () => {
+    expect(1 + 1).to.equal(2)
   })
+
+  describe('nested', () => {
+    before(() => { console.log('before nested') })
+    after(() => { console.log('after nested') })
+    beforeEach(() => { console.log('beforeEach nested') })
+    afterEach(() => { console.log('afterEach nested') })
+
+    helper('bark', function() { return 'woof' })
+
+    it('can echo', function() {
+      this.name = 'Latte'
+      expect(this.echoBot.welcome()).to.equal('Hello Latte')
+    })
+
+    it('can bark', function() {
+      expect(this.bark()).to.equal('woof')
+    })
+  })
+  // it('doesnt timeout on breakpoints', () => {
+  //   expect(1).to.equal(1)
+  //   debugger
+  //   expect(1).to.equal(1)
+  // })
+
+  // it('has a promise that throws an error', function() {
+  //   let prm = new Promise(resolve => {
+  //     setTimeout(resolve, 10)
+  //   })
+  //   let ret = prm.then(() => { throw new Error('BANG') })
+  //   return ret
+  // })
 })
