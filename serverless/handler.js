f80604fabc56f79a7e55a59ae6e54a81012f5830
rogueg <grant@superhuman.com>
2019-04-25 00:52:07 -0700

    Faster syncing to S3 from new devices
    Previously, we'd use an on-disk cache to keep track of what files we had already sent to S3, and what changes we need to upload.
    
    This doesn't well on a CI server where the disk is more ephemeral. We'd spend a lot of time (not to mention S3 storage) saving the same files over and over. Instead, we should let clients share files when they're unchanged.
    
    Clients generate a manifest of all files and their hashes. The `sync` lambda function takes that and figures out what files are missing from S3 so we can upload them.
    
    When running tests, we use the manifest again to rewrite generic requests to their version-specific ones in S3.  (/lib/latte.js => lib/latte-1e69e8b91.js).
    
    I experimented with a lot of different approaches to make this work. The key constraint was performance: in the span of 1-2 seconds we want to serve 10GB across 10k requests. S3 is actually pretty good at that. I also tried an in-memory node server, lambda@edge, and the API gateway. They were all slow, or we'd exceed their rate limits.

--- serverless/handler.js
+++ serverless/handler.js
@@ -1,19 +1,27 @@
 const AWS = require('aws-sdk')
-const crypto = require('crypto')
 
-module.exports.workTests = async (opts, context) => {
+module.exports.workTests = async (opts, context, callback) => {
   const launchChrome = require('@serverless-chrome/lambda')
   const ChromeWrapper = require('./lib/chrome')
-  let chrome, remaining = opts.testNames.slice(), results = []
+  let chrome, manifest, remaining = opts.testNames.slice(), results = []
   delete opts.testNames
 
   try {
-    console.log('Launching chrome')
-    chrome = await launchChrome({})
+    console.log('Starting')
+    await Promise.all([
+      launchChrome({}).then(c => chrome = c),
+      getManifest(opts.Bucket, opts.sessionId).then(m => manifest = m)
+    ])
+
+    console.log('Opening tab')
     let wrapper = new ChromeWrapper({})
     wrapper.connectToRunning()
-    console.log('Opening tab')
     let tab = await wrapper.openTab(opts.url)
+
+    if (opts.sessionId) { // temp to handle old clients
+      if (!manifest) throw new Error(`Missing manifest for ${opts.sessionId}`)
+      tab.manifest = manifest
+    }
 
     console.log('Starting tests')
     while (remaining.length > 0) {
@@ -24,46 +32,80 @@ module.exports.workTests = async (opts, context) => {
     }
 
     chrome.kill()
-    return {statusCode: 200, body: results}
+    callback(null, {statusCode: 200, body: results})
 
   } catch (e) {
     console.error(e)
     e.logStream = context.logStreamName
     chrome && chrome.kill()
-    throw e
+    callback(e)
   }
 }
 
+module.exports.sync = async (manifest) => {
+  let s3 = new AWS.S3({params: {Bucket: manifest.bucket}})
 
-module.exports.applyPatch = (opts, context, callback) => {
-  const DiffMatchPatch = require('diff-match-patch')
-  let dmp = new DiffMatchPatch()
-  let patches = dmp.patch_fromText(opts.patch)
-  let s3 = new AWS.S3()
+  // Write the updated session manifest to S3
+  let manifestWrite = s3.putObject({
+    Key: `session-${manifest.sessionId}.json`,
+    Body: JSON.stringify(manifest)
+  }).promise()
 
-  s3.getObject({Bucket: opts.bucket, Key: opts.key}).promise().then(obj => {
-    // TODO make this actually work. patch_apply mostly seems to work, so this doesn't feel urgent
-    // let md5 = crypto.createHash('md5').update(obj.Body).digest()
-    // if (md5 != opts.md5)
-    //   return callback('stale md5')
+  // List out all the files we already have in S3
+  let cached = [], listOpts = {}
+  while (listOpts) {
+    console.log('Listing objects', listOpts)
+    let resp = await s3.listObjectsV2(listOpts).promise()
+    console.log('Got resp', resp)
+    cached.push.apply(cached, resp.Contents.map(c => c.Key))
+    listOpts.ContinuationToken = resp.NextContinuationToken
+    if (!resp.IsTruncated) listOpts = null
+  }
+  console.log(`Found ${cached.length} items in S3`)
 
-    let [output, status] = dmp.patch_apply(patches, obj.Body)
-
-    if (!status.every(v => !!v))
-      return callback('patch application failed')
-
-    return s3.upload({
-      Bucket: opts.bucket,
-      Key: opts.key,
-      ContentType: obj.ContentType,
-      Body: output
-    }).promise()
-
-  }).then(() => {
-    callback(null, {success: true})
-
-  }).catch(e => {
-    console.error(e)
-    callback(e)
+  // Figure out which ones are specified in the manifest that we don't have
+  let files = Object.keys(manifest.files).map(p => {
+    let key = manifest.files[p]
+    return {path: p, key, needed: cached.indexOf(key) === -1}
   })
+  let needed = files.filter(f => f.needed)
+  console.log(`Need ${needed.length} files to run tests`)
+
+  await manifestWrite
+  console.log('Manifest written')
+  return {needed}
+}
+
+module.exports.routeRequest = async (event) => {
+  let [Bucket, sessionId, ...rest] = event.path.split('/').slice(1)
+  let manifest = await getManifest(Bucket, sessionId)
+  let path = decodeURIComponent(rest.join('/'))
+  console.log('Routing', Bucket, sessionId, path)
+
+  if (!manifest) {
+    return {statusCode: 404, headers: {}, body: 'manifest not found'}
+  }
+
+  if (path === 'index.html') {
+    return {statusCode: 200, headers: {"content-type": "text/html"}, body: manifest.index}
+  }
+
+  let key = manifest.files[path]
+  if (!key) {
+    return {statusCode: 404, headers: {}, body: 'path not found in manifest'}
+  }
+
+  return {statusCode: 301, headers: {Location: `https://s3-us-west-2.amazonaws.com/${Bucket}/${encodeURIComponent(key)}`}}
+}
+
+async function getManifest (Bucket, sessionId) {
+  if (!sessionId) return // temp to support old clients
+  try {
+    let s3 = new AWS.S3()
+    let resp = await s3.getObject({Bucket, Key: `session-${sessionId}.json`}).promise()
+    return JSON.parse(resp.Body.toString('utf-8'))
+  } catch (e) {
+    console.log(e)
+    return null
+  }
 }
